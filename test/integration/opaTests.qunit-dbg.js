ee4251cbcde1e3eec141f96ce438bbf7ee5f202f
Carlos-queiroz-abap <cadusqueiroz@yahoo.com>
2026-05-15 12:47:23 -0300

    Delete test directory

--- test/integration/opaTests.qunit-dbg.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["curriculo/project1/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- test/unit/AllTests.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["curriculo/project1/test/unit/controller/View1.controller"],function(){"use strict"});
-//# sourceMappingURL=AllTests.js.map
